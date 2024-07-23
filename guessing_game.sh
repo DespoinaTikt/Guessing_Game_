f569fc84f7deb7e370e97b4c76a8b39862dafd02
Despoina Tiktopoulou <166096217+DespoinaTikt@users.noreply.github.com>
2024-07-23 11:46:46 +0300

    Add files via upload

---
+++ guessing_game.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+COMPUTER=50
+PLAYING=0
+while [ $PLAYING -eq 0 ]
+do
+	read -p "Pick a number: " INPUT
+	if [ $INPUT -eq $COMPUTER ]
+	then
+		echo "You've won, the number was $COMPUTER"
+		exit 0
+	elif [ $INPUT -lt $COMPUTER ]
+	then
+		echo "You're too low"
+	else
+		echo "You're too high"
+	fi
+done
+
+
+exit 0
